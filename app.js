30b00fe0aa17264058accd15120f5ad50fe42ddd
kafsalon <jfglamport@gmail.com>
2018-06-26 08:49:39 +0400

    exo 8 affichage du prenom

---
+++ app.js
@@ -0,0 +1,36 @@
+// chargement du module express
+const express = require('express');
+// initialisation du serveur express
+const app = express();
+
+const port = process.env.PORT || 3009;
+
+// chemin vers les fichiers static css, js appeler via le front
+app.use(express.static('static'));
+
+
+// utilisation du moteur de rendu ejs
+app.set('view engine', 'ejs');
+
+
+//  route principale qui affichera le nom et le prenom sur la page index via ejs
+app.get('/', function (req, res) {
+    res.render('index', {
+        name: name("Jean-fred")
+    });
+});
+
+/**
+ * fonction qui retoune le nom et prenom en parametre
+ * @param {string} first 
+ * @param {string} last 
+ */
+function name(first= "luke") {
+    return first ;
+}
+
+
+// start the server
+app.listen(port,  function () {
+    console.log('Démarrage du serveur sur =>  http://localhost:' + port);
+});
